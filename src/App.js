ee499875988f509233ba2c6c096216907df70184
Ralph Dipietro <rsdipietro@gmail.com>
2020-12-01 20:02:53 -0500

    go time

--- src/App.js
+++ src/App.js
@@ -2,31 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-<<<<<<< HEAD
-function App() {
-  debugger;
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
-=======
 
 console.log("test");
 console.log("d");
@@ -59,5 +35,3 @@ function App() {
 }
 
 export default App;
-
->>>>>>> eede90db83f346a309a3ace6aff5c82035da9285
